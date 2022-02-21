d90d9cbc1eb4c3a01aed9a8e15208b6b1cfbb61b
Ananpiya134 <anan.piyapanee@gmail.com>
2022-02-21 22:08:26 +0700

    add google_api

--- config/config.js
+++ config/config.js
@@ -1,34 +1,9 @@
-require("dotenv").config();
 
-<<<<<<< HEAD
-{
-	development: {
-		username: "root";
-		password: "0988319563o";
-		database: "sportify";
-		host: "127.0.0.1";
-		dialect: "mysql";
-	}
-	test: {
-		username: "root";
-		password: null;
-		database: "database_test";
-		host: "127.0.0.1";
-		dialect: "mysql";
-	}
-	production: {
-		username: "root";
-		password: null;
-		database: "database_production";
-		host: "127.0.0.1";
-		dialect: "mysql";
-	}
-}
-=======
+
 module.exports = {
   development: {
     username: "root",
-    password: process.env.MYSQL_PASSWORD,
+    password: "0988319563o",
     database: "sportify",
     host: "127.0.0.1",
     dialect: "mysql",
@@ -48,4 +23,3 @@ module.exports = {
     dialect: "mysql",
   },
 };
->>>>>>> 82c94e6e3a99c782738b4e0553d0fa1e36b437e2
