54312a54ba239fee88a06d05050bd82baec9f537
Ananpiya134 <anan.piyapanee@gmail.com>
2022-03-03 15:02:36 +0700

    change actvity seeds

--- seeders/20220217154916-activity.js
+++ seeders/20220217154916-activity.js
@@ -1,49 +1,82 @@
 "use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    // const hashedPassword = await bcrypt.hash("1234", 10);
-    return queryInterface.bulkInsert("activities", [
-      {
-        id: "1",
-        name: "Basketball Party Night",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        id: "2",
-        name: "Lasergame Party Night",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        id: "3",
-        name: "Football Party Night",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        id: "4",
-        name: "Rockclimbing Party Night",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        id: "5",
-        name: "Tennis Party",
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
-  },
+	up: async (queryInterface, Sequelize) => {
+		// const hashedPassword = await bcrypt.hash("1234", 10);
+		return queryInterface.bulkInsert("activities", [
+			{
+				id: 1,
+				name: "Cycling",
+				icon: "fa-solid fa-person-biking",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				id: 2,
+				name: "Basketball",
+				icon: "fa-solid fa-basketball",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				id: 3,
+				name: "Golf",
+				icon: "fa-solid fa-golf-ball-tee ",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				id: 4,
+				name: "Boxing",
+				icon: "fa-solid fa-basketball",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				id: 5,
+				name: "Tennis",
+				icon: "fa-solid fa-table-tennis-paddle-ball",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				id: 6,
+				name: "Fitness",
+				icon: "fa-solid fa-dumbbell",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				id: 7,
+				name: "Volleyball",
+				icon: "fa-solid fa-volleyball",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				id: 8,
+				name: "Football",
+				icon: "fa-solid fa-football",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				id: 9,
+				name: "Other",
+				icon: "fa-solid fa-ellipsis",
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+		]);
+	},
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    await queryInterface.bulkDelete("activities", null, {});
-  },
+	down: async (queryInterface, Sequelize) => {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
+		await queryInterface.bulkDelete("activities", null, {});
+	},
 };
