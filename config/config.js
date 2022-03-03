b66c1ea762a6a693c81713ea6308250e6536f153
Ananpiya134 <anan.piyapanee@gmail.com>
2022-03-03 14:25:10 +0700

    error

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  development: {
-    username: "root",
-    password: "0810383436",
-    database: "sportify",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+	development: {
+		username: "root",
+		password: "0988319563o",
+		database: "sportify",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+	test: {
+		username: "root",
+		password: null,
+		database: "database_test",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+	production: {
+		username: "root",
+		password: null,
+		database: "database_production",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
 };
