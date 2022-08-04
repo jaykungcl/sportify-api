8e49da7d413dfae3c8687e6e3e264738ce115b29
Ananpiya134 <anan.piyapanee@gmail.com>
2022-08-04 22:51:23 +0700

    commit for changing to pc

--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
+require("dotenv").config();
+
 module.exports = {
-  development: {
-    username: "root",
-    password: "Satl9131",
-    database: "sportify",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+	development: {
+		username: "root",
+		password: process.env.MY_SQL_PASSWORD,
+		database: "sportify",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+	test: {
+		username: "root",
+		password: null,
+		database: "database_test",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
+	production: {
+		username: "root",
+		password: null,
+		database: "database_production",
+		host: "127.0.0.1",
+		dialect: "mysql",
+	},
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,52 +7,52 @@ const { User } = require("../models");
 const uploadPromise = util.promisify(cloudinary.uploader.upload);
 
 exports.getProfile = async (req, res, next) => {
-  const { id: userId } = req.params;
-  try {
-    const user = await getUserById(userId);
-    return res.status(200).json({ user });
-  } catch (err) {
-    next(err);
-  }
+	const { id: userId } = req.params;
+	try {
+		const user = await getUserById(userId);
+		return res.status(200).json({ user });
+	} catch (err) {
+		next(err);
+	}
 };
 
 exports.updateBio = async (req, res, next) => {
-  const id = req.params.id;
-  const body = req.body;
+	const id = req.params.id;
+	const body = req.body;
 
-  if (!body.bio) res.send({ status: false, msg: "Params bio is not valid." });
+	if (!body.bio) res.send({ status: false, msg: "Params bio is not valid." });
 
-  try {
-    const user = await updateBioById(id, body.bio);
-    if (user[0] < 1)
-      return res.send({ status: false, msg: "Update bio fail." });
+	try {
+		const user = await updateBioById(id, body.bio);
+		if (user[0] < 1)
+			return res.send({ status: false, msg: "Update bio fail." });
 
-    return res.send({ status: true, msg: "Update bio success." });
-  } catch (err) {
-    //     next(err);
-    return res.status(400).send("Somethin wrong");
-  }
+		return res.send({ status: true, msg: "Update bio success." });
+	} catch (err) {
+		//     next(err);
+		return res.status(400).send("Somethin wrong");
+	}
 };
 
 exports.updateProfilePic = async (req, res, next) => {
-  const id = req.params.id;
-  const user = await User.findOne({ where: { id } });
-  if (!user) {
-    res.status(400).json({ message: "user not found" });
-  }
+	const id = req.params.id;
+	const user = await User.findOne({ where: { id } });
+	if (!user) {
+		res.status(400).json({ message: "user not found" });
+	}
 
-  if (req.file) {
-    const { path } = req.file;
-    if (user.imgUrl) {
-      const splited = user.imgUrl.split("/");
-      cloudinary.uploader.destroy(splited[splited.length - 1].split(".")[0]);
-    }
+	if (req.file) {
+		const { path } = req.file;
+		if (user.imgUrl) {
+			const splited = user.imgUrl.split("/");
+			cloudinary.uploader.destroy(splited[splited.length - 1].split(".")[0]);
+		}
 
-    const result = await uploadPromise(path);
-    fs.unlinkSync(path);
+		const result = await uploadPromise(path);
+		fs.unlinkSync(path);
 
-    user.update({ imgUrl: result.secure_url });
+		user.update({ imgUrl: result.secure_url });
 
-    res.status(200).json({ user });
-  }
+		res.status(200).json({ user });
+	}
 };
